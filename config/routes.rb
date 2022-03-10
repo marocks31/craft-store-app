9ebcda49cfc6a838d89de96ba8209faf22be4623
marocks31 <mollyarisley@gmail.com>
2022-03-10 13:14:05 -0600

    added craft_supplies route and anmed controller 'supplies'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  get 
+get  "/craft_supplies", controller: "supplies", action: "supplies_method"
 end
