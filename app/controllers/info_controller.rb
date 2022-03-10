b1d60157c2391f4ecb618d9287b889beb0eb03af
marocks31 <mollyarisley@gmail.com>
2022-03-10 13:58:16 -0600

    struggled to make url_segment_params, added craft_ideas route and craft_idea_method controller

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -5,4 +5,9 @@ def supplies_method
   material_two = params["material2"]
 render json: {message: "The materials you requested are #{material_one} and #{material_two}. "}
 end
+
+def craft_idea_method
+  idea_one = params["idea1"]
+   render json: {message: "Cool you should make a #{idea_one}"}
+end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 
   # http://localhost:3000/craft_supplies?material1=paper&material2=glue
 get  "/craft_supplies", controller: "info", action: "supplies_method"
-end
+
+
+#url_segment_params
+get "/craft_ideas", controller: "info", action: "craft_idea_method"
+end 
